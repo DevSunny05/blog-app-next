018b2f309fbf34c53c2f42223629eab0ce066d82
sarang <sarang.office01@gmail.com>
2025-11-25 15:30:24 +0530

    fixed some bugs

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import { ClerkProvider } from "@clerk/nextjs";
 import Footer from "@/components/Footer";
+import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <Header />
+          <Suspense fallback={<div className="min-h-16" />}>
+            <Header />
+          </Suspense>
           {children}
           <Footer/>
         </body>
