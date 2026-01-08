670e1880ec90479e5da22ea5a78e2febcb8654a9
sarang <sarang.office01@gmail.com>
2026-01-08 23:27:46 +0530

    new files has been added

--- src/lib/mongoDB/config.js
+++ src/lib/mongoDB/config.js
@@ -8,9 +8,9 @@ export const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     isConnected = true;
-    console.log("✅ MongoDB connected");
+    console.log(" MongoDB connected");
   } catch (error) {
-    console.error("❌ MongoDB connection error :", error);
+    console.error(" MongoDB connection error :", error);
     throw new Error("MongoDB connection failed");
   }
 };
